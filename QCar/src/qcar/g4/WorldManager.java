3b5928744ab767adf72f52cca783ad355fd3ebe0
Karim <Karim@mbp.sofr.hefr.lan>
2017-03-27 13:05:58 +0200

    Ajout des methodes add, remove et openNewSimulation au worldmanager

--- QCar/src/qcar/g4/WorldManager.java
+++ QCar/src/qcar/g4/WorldManager.java
@@ -1,6 +1,7 @@
 package qcar.g4;
 
 import java.awt.geom.Line2D;
+import java.util.ArrayList;
 import java.util.List;
 
 import javafx.geometry.Rectangle2D;
@@ -11,43 +12,51 @@ import simviou.WorldChangeObserver;
 public class WorldManager implements IWorldManager, ObserverRegistrar {
 
   private List<WorldChangeObserver> observers;      // contains all currently observing objects
-
+  private long step;                                // number of step played
+  private boolean isSimulationRunning;              // true if simulation is running else false
 
   @Override
   public void addWorldObserver(WorldChangeObserver o) {
-    // TODO Auto-generated method stub
     if(o != null && !observers.contains(o))
-      observers.add(o);
+      this.observers.add(o);
   }
 
   @Override
   public void removeWorldObserver(WorldChangeObserver o) {
-    // TODO Auto-generated method stub
-    if(observers.contains(o))  
-      observers.remove(o);
+    this.observers.remove(o);
   }
 
   @Override
   public void openNewSimulation(IGameDescription description, List<? extends IDriver> players) {
     // TODO Auto-generated method stub
-    
+    // add each qcar to the observer list
+    for (IQCar qcar : description.allQCar()) {
+      addWorldObserver((WorldChangeObserver) qcar);
+    }
+    // add each driver to the observer list
+    for (IDriver driver: players) {
+      addWorldObserver((WorldChangeObserver) driver);
+    }
   }
 
   @Override
   public boolean isSimulationOpened() {
-    // TODO Auto-generated method stub
-    return false;
+    return isSimulationRunning;
   }
 
   @Override
   public void simulateOneStep(long collectiveDelayInMicroSeconds) {
     // TODO Auto-generated method stub
-
+    step++;
+    for (WorldChangeObserver o: observers) {
+      o.notify();
+    }
   }
 
   @Override
   public void closeSimulation() {
     // TODO Auto-generated method stub
+    isSimulationRunning = false;
     for(int i = 0; i < observers.size(); i++)
       observers.remove(i);
   }
@@ -73,25 +82,40 @@ public class WorldManager implements IWorldManager, ObserverRegistrar {
   @Override
   public List<IQCar> allQCars() {
     // TODO Auto-generated method stub
-    return null;
+    List<IQCar> allQCars;
+    for(int i = 0; i < observers.size(); i++){
+      // if(observers.get(i) instanceof IQCar)
+        // allQCars.add((IQCar) observers.get(i));
+    }
+    return null; //allQCars;
   }
 
   @Override
   public List<Line2D> allPhotoSensors() {
     // TODO Auto-generated method stub
-    return null;
+    List<Line2D> allPhotoSensors = null;
+    
+    return allPhotoSensors;
   }
 
   @Override
   public List<ICollision> allNewCollisions() {
     // TODO Auto-generated method stub
-    return null;
+    List<ICollision> allNewCollisions = null;
+    
+    return allNewCollisions;
   }
 
   @Override
   public List<Line2D> allDistanceSensors() {
     // TODO Auto-generated method stub
-    return null;
+    List<Line2D> allDistanceSensors = null;
+    
+    return allDistanceSensors;
+  }
+
+  public WorldManager(){
+    this.observers = new ArrayList<>();
   }
 
 }
