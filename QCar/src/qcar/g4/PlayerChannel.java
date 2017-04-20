b708e16951b5c20c17da2e618c83e637beade54a
Vonlanth <Vonlanth@Vonlanth-PC.sofr.hefr.lan>
2017-04-20 16:11:21 +0200

    minor changes in Driver and comments in PlayerChannel

--- QCar/src/qcar/g4/PlayerChannel.java
+++ QCar/src/qcar/g4/PlayerChannel.java
@@ -5,8 +5,14 @@ import qcar.IPlayerChannel;
 import qcar.ISensors;
 
 public class PlayerChannel implements IPlayerChannel {
+  IDecision decision ;
+  ISensors sensors ;
   @Override
-  public ISensors play(IDecision decision) {    
+  public ISensors play(IDecision decision) {  
+    // 1 : reçoit une décision du Driver
+    // 2 : envoie la décision au WorldManager
+    // 3 : BLOCK : attend les senseurs du WorldManager
+    // 4 : envoie les senseurs au Driver
     return null;
   }
 
