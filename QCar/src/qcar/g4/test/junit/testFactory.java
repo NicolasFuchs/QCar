eac955dd0a75e2801dd58c04550f07e84dd216f9
Sargis <bouteille.cedric@yahoo.fr>
2017-04-21 15:51:15 +0200

    testFactory and testGameProvider added

---
+++ QCar/src/qcar/g4/test/junit/testFactory.java
@@ -0,0 +1,63 @@
+package qcar.g4.test.junit;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import qcar.ApiTest;
+import qcar.IFactory;
+import qcar.IGameDescription;
+import qcar.IGameProvider;
+
+public class testFactory extends ApiTest {
+  
+  public testFactory(IFactory fact, IFactory aux) {
+    super(fact, aux);
+  }
+  
+  @Test
+  public void testNumberOfPlayers(int maxPlayers){
+    assertTrue(maxPlayers>0);
+    IFactory f=factoryUnderTest;
+    int nStyles = f.numberOfStyles();
+    assertTrue("number of styles must be positive", nStyles > 0);
+    IGameProvider g = f.newGameProvider(0);
+    IGameDescription gd;
+
+    for(int i=1; i<maxPlayers; i++){
+      gd = g.nextGame(i);
+      assertTrue(!gd.allQCar().isEmpty());
+      assertTrue(gd.allQCar().size()==i);
+    }
+  }
+  
+  // ---------------------------------------------------------------------------
+  // this tests have been recuperated from TinyFactoryTest class
+  @Test
+  public void allStylesExist(){
+    IFactory f=factoryUnderTest;
+    int nStyles = f.numberOfStyles();
+    int nPlayers=2;
+    assertTrue("number of styles must be positive", nStyles > 0);
+    for(int i=0; i<nStyles; i++) {
+      IGameProvider g = f.newGameProvider(i);
+      // Now just to verify that it is able to build a game:
+      assertTrue(g.nextGame(nPlayers) != null); 
+    }
+  }
+
+  @Test
+  public void testFactoryInstances(){
+    int style=0;
+    assertTrue(factoryUnderTest.newGameProvider(style)!=null);
+    assertTrue(factoryUnderTest.newSmartDriver()!=null);
+    assertTrue(factoryUnderTest.newWorldManager()!=null);
+  }
+  // ---------------------------------------------------------------------------
+
+  
+  public static void main(String[] args) {
+
+
+  }
+
+}
