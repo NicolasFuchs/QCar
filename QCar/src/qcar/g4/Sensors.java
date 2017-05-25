9a7ae46a1c21c12bbb80f77cb7f8d4145fa93472
Nicolas Fuchs <Nicolas Fuchs>
2017-05-25 23:34:24 +0200

    javadoc added for Sensors

--- QCar/src/qcar/g4/Sensors.java
+++ QCar/src/qcar/g4/Sensors.java
@@ -3,6 +3,9 @@ package qcar.g4;
 import java.util.List;
 import qcar.*;
 
+/**
+ * Describes what a QCar can see around him
+ */
 public class Sensors implements ISensors {
   
   QCar mySelf;
@@ -10,6 +13,13 @@ public class Sensors implements ISensors {
   List<ICollision> colWithMe;
   IDistanceSensor distanceSensor;
   
+  /**
+   * Constructor of a Sensor
+   * @param myself
+   * @param sv
+   * @param cwm
+   * @param distanceSensor
+   */
   public Sensors(IQCar myself, List<ISeenVertex> sv, List<ICollision> cwm, IDistanceSensor distanceSensor) {
     this.mySelf = (QCar)myself;
     this.seenVert = sv;
@@ -17,21 +27,33 @@ public class Sensors implements ISensors {
     this.distanceSensor = distanceSensor;
   }
 
+  /**
+   * @return List of collisions with me involved
+   */
   @Override
   public List<ICollision> collisionsWithMe() {
     return this.colWithMe;
   }
 
+  /**
+   * @return List of vertices seen by the side0
+   */
   @Override
   public List<ISeenVertex> seenVertices() {
     return this.seenVert;
   }
 
+  /**
+   * @return QCar who's having this sensor
+   */
   @Override
   public IQCar mySelf() {
     return this.mySelf;
   }
 
+  /**
+   * @return the smallest distance between me and the closest QCar or boundingBox
+   */
   @Override
   public IDistanceSensor distanceSensor() {
     return distanceSensor;
