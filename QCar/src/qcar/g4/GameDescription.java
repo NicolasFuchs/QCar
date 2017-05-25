431cd9a8f633575641a3c72d8a312724ccd73586
Sargis <bouteille.cedric@yahoo.fr>
2017-05-25 23:05:11 +0200

    Javadoc for GameDescritpion

--- QCar/src/qcar/g4/GameDescription.java
+++ QCar/src/qcar/g4/GameDescription.java
@@ -8,6 +8,24 @@ import qcar.IQCar;
 
 public class GameDescription implements IGameDescription{
   
+  /* Enumeration of the styles of games available
+   * 
+   * The first QCars are always those being driven
+   * 
+   * MIXED_WITH_BORDERS, game with mixed driven, static and parking QCars and the last QCar
+   *                     is a static QCar representing the border
+   * ONLY_DRIVERS_WITH_BORDERS, game with only driven QCars and the last QCar is a static QCar 
+   *                            representing the border
+   * NO_STATICS_WITH_BORDERS, game with only driven and parking QCars and the last QCar is a static
+   *                          QCar representing the border 
+   * NO_PARKINGS_WITH_BORDERS, game with only driven and static QCars and the last QCar is a static
+   *                           QCar representing the border 
+   * MIXED_WITHOUT_BORDERS, game with mixed driven, static and parking QCars
+   * ONLY_DRIVERS_WITHOUT_BORDERS, game with only driven QCars
+   * NO_STATICS_WITHOUT_BORDERS, game with only driven and parking QCars
+   * NO_PARKINGS_WITHOUT_BORDERS, game with only driven and static QCars
+   * 
+   */ 
   public enum GameStyles {
     MIXED_WITH_BORDERS,
     ONLY_DRIVERS_WITH_BORDERS,
@@ -21,10 +39,20 @@ public class GameDescription implements IGameDescription{
   
   private ArrayList<IQCar> cars;
   
+  /* Method to create a new GameDescription
+   * Initialize the GameDescription with list of the QCar given in parameters
+   * 
+   *    @param  : ArrayList<IQCar>, the list of the QCars representing this game
+   */
   public GameDescription(ArrayList<IQCar> cars) {
     this.cars = cars;
   }
   
+  /* Method to return the list of QCars describing this game
+   * 
+   *    @param  : -
+   *    @return : List<IQCar>, the list of the QCars representing this game
+   */
   @Override
   public List<IQCar> allQCar() {
     return cars;
